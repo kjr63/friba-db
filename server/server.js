675501cf9329c369894de33e52ecd2d988de8a68
Kaj Risberg <kaj.risberg@helsinkinet.fi>
2020-06-12 11:35:32 +0300

    muokattu heroku asetuksia 3

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public_html');
-//const http = require('http');
-const port = process.env.PORT;
+const http = require('http');
+const port = process.env.PORT || staticPort; //heroku asetus process.env.PORT
 
 app.use(express.static(publicPath));
 app.listen( port, () => { console.log('friba-db is alive!'); } );
